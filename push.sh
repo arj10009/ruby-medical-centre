4d3e56efd9be5b67ff02efcc9a03bf2b1c33e351
Arjun <chakravyuga1@gmail.com>
2026-03-15 12:20:01 +0800

    Remove scripts with tokens

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-TOKEN="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"
-git add -A
-git commit -m "Link Book an Appointment button to HealthEngine"
-git remote set-url origin "https://arj10009:${TOKEN}@github.com/arj10009/ruby-medical-centre.git"
-git push origin main
-echo "Updated! Changes will be live in about 30 seconds."
